a65748f6b159e2fd1a9935f63b9ec63d736d7325
Kwonkunkun <kunwoo5012@naver.com>
2023-01-15 16:49:59 +0900

    chore(setting): db setting
    - typeorm setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
-import { HealthModule } from "./modules/health/health.module";
+import { HealthModule } from './modules/health/health.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from '@nestjs/config';
+import { User } from './typeorm/entities/user';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          entities: [User],
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    HealthModule,
+  ],
   providers: [],
 })
 export class AppModule {}
